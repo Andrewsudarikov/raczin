eff2b0b98fa6e950afa98c456220ba5df68f81de
Andrei Sudarikov <andrewsudarikov@gmail.com>
2021-09-11 08:15:53 +0300

    Removed unused modules and namesapces from the code.

--- Raczin.py
+++ Raczin.py
@@ -1,10 +1,9 @@
 import csv
 import gi
 import random
-import math
 gi.require_version('Gtk', '3.0')
 gi.require_version('Granite', '1.0')
-from gi.repository import Gtk, Gdk, Gio, GLib, Granite, Pango
+from gi.repository import Gtk, Gdk, Gio, Granite, Pango
 
 # Setting up the difficulty level
 global Difficulty_Level
